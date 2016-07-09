db432def0489b3d6d6c6436857f54599fdff845a
vinothwindows47 <vinothwindows47@gmail.com>
2016-07-09 13:26:09 +0530

    Update editor.js

--- editor.js
+++ editor.js
@@ -9,19 +9,19 @@
 </div>
 <div class="debugtabHeader">
 	<div class="debugHeadIcons" id="variableViewIcons" style="display: block;">
-		<span title="terminate" class="layouttop-rightMenuOption debugiconmenu" {{ action "debuggingAction" "debugStop" }}>
+		<span title="terminate" class="layouttop-rightMenuOption debugiconmenu" {{ action "debuggingAction" "STOP" }}>
 			<div class="topbar-ui-icons icon-debug-stop {{if debugControllers.debug 'icon-debug-terminateenable' 'icon-debug-disable'}}" style="font-size:20px;margin:5px 0px 0px 5px;"></div>
 		</span>
-		<span title="Resume" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "debugResume" }}>
+		<span title="Resume (F8)" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "RESUME" }}>
 			<div class="topbar-ui-icons fsize16 fa fa-eject tform90" style="top: 3px;"></div>
 		</span>
-		<span title="step over" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "debugStepOver"}} >
+		<span title="Step Over (F6)" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "STEPOVER"}} >
 			<div class="topbar-ui-icons fsize16 icon-debug-step-over"></div>
 		</span>
-		<span title="step into" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "debugStepInto"}} >
+		<span title="Step Into (F5)" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "STEPINTO"}} >
 			<div class="topbar-ui-icons fsize16 icon-debug-step-into" ></div>
 		</span>
-		<span title="step out" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "debugStepOut"}} >
+		<span title="Step Out" class="layouttop-rightMenuOption debugiconmenu {{if debugControllers.next 'icon-debug-enable' 'icon-debug-disable'}}" {{ action "debuggingAction" "STEPOUT"}} >
 			<div class="topbar-ui-icons fsize16 icon-debug-step-out"></div>
 		</span>
 		{{#debug/show-allconfig thisCtrl="window/debug"}}
@@ -31,7 +31,7 @@
 	      			<span class=""><span class="fa fa-caret-down"></span></span>
 	  			</span>
 			</span>
-			<span class="layouttop-rightMenuOption curPointer" id="runshowAllConfig" title="Run As" style="padding: 7px 7px 8px;" {{action "setdebugActionType" "run"}}>
+			<span class="layouttop-rightMenuOption curPointer" id="runshowAllConfig" title="Run As" style="padding: 7px 7px 8px; display:none" {{action "setdebugActionType" "run"}}>
 	  			<span class="runAsDv">
 	      			<span class="runImgC fa fa-play-circle-o"></span>
 	      			<span class=""><span class="fa fa-caret-down"></span></span>
@@ -39,46 +39,23 @@
 			</span>
 		{{/debug/show-allconfig}}
 	</div>
-	<div class="debugHeadIcons" id="bpointViewIcons">
-		<span class="layouttop-rightMenuOption debugiconmenu {{if chkbpointItems 'icon-debug-enable' 'icon-debug-disable'}}" {{action "removeall_bpoint"}}>
-			<div class="topbar-ui-icons fsize16 fa fa-times-circle bpointClose" title="Remove All Breakpoints"></div>
-		</span>
-		<span class="layouttop-rightMenuOption debugiconmenu {{if chkselectedbpointItems 'icon-debug-enable' 'icon-debug-disable'}}" {{action "removeSelected_bpoint"}}>
-			<div class="topbar-ui-icons fsize16 fa fa-times bpointClose" title="Remove Selected Breakpoints"></div>
-		</span>
-	</div>
 </div>
 {{#debug/debug-layout thisCtrl="window/debug"}}
-	<div id="debugMainView" class="debugMainView">
-		<div id="debugThreadView">
-			<div class="debugnavtabHeader navtabHeader">
-				<ul class="navtabItems">
-					{{#each debugMenuItems.left as |debugitem|}}
-						<li id="{{debugitem.id}}" class="navtab-item {{if debugitem.active 'navActive'}}" {{ action "debugAction" debugitem "left" }}>
-							<a>{{debugitem.item}}</a>
-						</li>
-					{{/each}}
-				</ul>
+	<div id="debugMainView" class="debugMainView" style='height:auto;'>
+		{{#each debugMenuItems as |debugItem|}}
+			<div id={{debugItem.id}}>
+				<div class='debugHeader' {{action "toggleLayout" debugItem}}>
+					<span class='bold' style='color:#616161'>
+						{{#if debugItem.expand}}
+							<i class="fa fa-caret-down" aria-hidden="true" style='padding-right:5px;'></i>
+						{{else}}
+							<i class="fa fa-caret-right" aria-hidden="true" style='padding-right:5px;'></i>
+						{{/if}}
+						{{debugItem.name}}
+					</span>
+				</div>
+				{{partial debugItem.partial}}
 			</div>
-			<div class="debugnavMainView">
-				{{partial debugLeftType}}
-			</div>
-		</div>
-		<div id="debugVariableView">
-			<div class="debugnavtabHeader navtabHeader">
-				<ul class="navtabItems">
-					{{#each debugMenuItems.center as |debugitem|}}
-						<li id="{{debugitem.id}}" class="navtab-item {{if debugitem.active 'navActive'}}" {{ action "debugAction" debugitem "center" }}>
-							<a>{{debugitem.item}}</a>
-						</li>
-					{{/each}}
-				</ul>
-			</div>
-			<div class="debugnavMainView">
-				{{partial debugCenterType}}
-			</div>
-		</div>
-		<div id="debugWatcherView">
-		</div>
+		{{/each}}
 	</div>
 {{/debug/debug-layout}}
